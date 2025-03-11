b1403f8dc8f88cc9b28db65590b5834dc3b3d2f8
0m4rP <omar17penagos@gmail.com>
2025-03-10 21:47:58 -0500

    finalización del backend y api

---
+++ SistemaVenta.DAL/Repositorios/Contrato/IGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace SistemaVenta.DAL.Repositorios.Contrato
+{
+    public interface IGenericRepository<TModel> where TModel : class
+    {
+        //metodos que van a trabajar con nuestros modelos CRUD
+
+        //trabajar de manera asincrona
+        //esto es para obtener un modelo
+        Task<TModel> Obtener(Expression<Func<TModel, bool>> filtro);
+        //recibir un nuevo modelo para crear
+        Task<TModel> Crear(TModel modelo);
+
+        Task<bool> Editar(TModel modelo);
+        Task<bool> Eliminar(TModel modelo);
+        //devuelve la consulta del modelo
+        Task<IQueryable<TModel>> Consultar(Expression<Func<TModel, bool>> filtro = null);
+
+
+    }
+}
